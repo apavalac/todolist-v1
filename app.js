5a29a000493f8d352f860d0fb19be50b63533b0f
apavalac <apavalac@student.42.fr>
2020-10-17 22:39:39 +0300

    Added a basic mechanic for to-do list

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let nItem = [
+	"Bazic item",
+	"Food"
+];
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) =>{
 	let today = new Date();
 
-	res.render("index", {test: "test"});
+	let options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long"
+	};
+
+	let day = today.toLocaleDateString("en-US", options);
+
+	res.render("index", {
+		day: day,
+		newListItem: nItem
+	});
+});
+
+app.post("/", (req, res) => {
+	nItem.push(req.body.newItem);
+
+	console.log(nItem);
+
+	res.redirect("/");
 });
 
 app.listen(3000, () => {
